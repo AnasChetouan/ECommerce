6ce02d14570698c0141656408889e4e5fe41cc6c
Anas Chetouan <anas.chetouan@gmail.com>
2019-11-08 10:50:58 +0100

    modif script sh

--- creationbase.sh
+++ creationbase.sh
@@ -1,2 +1,2 @@
 mongoimport --db ESTELLE --collection membres --file collections/membres.json --jsonArray --drop
-mongoimport --db ESTELLE --collection produit --file collections/produits.json --jsonArray --drop
+mongoimport --db ESTELLE --collection produits --file collections/produits.json --jsonArray --drop
