88facdf4080769380db26f75fd73f7e9e7edf7b9
Di Marino Marco <marco.di-marino@etu.umontpellier.fr>
2019-11-15 11:58:42 +0100

    categorie component

--- ESTELLE/src/app/categories/categories.component.ts
+++ ESTELLE/src/app/categories/categories.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthentificationService } from '../authentification.service';
+import { Router } from '@angular/router';
+import { ProduitService } from '../produits.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  private user : Observable<string>;
+  private categories: String[] = new Array;
+  constructor(private router: Router,
+              private authService : AuthentificationService,
+              private produitService : ProduitService) {
+          this.user = this.authService.getUser();
+  }
 
   ngOnInit() {
+    this.produitService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    })
+  }
+
+  produitsParCategorie(categorie){
+    this.router.navigate(['/produits', categorie]);
   }
 
 }
